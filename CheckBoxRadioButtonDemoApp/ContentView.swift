7517d08684ee2326b75af09eaff228f9a0520b7b
ryota <k014c1106@it-neec.jp>
2023-01-11 21:30:46 +0900

    initial commit

--- CheckBoxRadioButtonDemoApp/ContentView.swift
+++ CheckBoxRadioButtonDemoApp/ContentView.swift
@@ -7,15 +7,58 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    @State var catToggle = false
+    @State var dogToggle = false
+    
+    @State private var selectedIndex = 0
+
+    private let selectNames = ["オス", "メス"]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+                    
+            Toggle("Cat", isOn: $catToggle)
+              .toggleStyle(CheckBoxToggleStyle())
+            Toggle("Dog", isOn: $dogToggle)
+              .toggleStyle(CheckBoxToggleStyle())
+
+            Spacer()
+                    
+                ForEach(0..<selectNames.count, id: \.self, content: { index in
+                    HStack {
+                        Text(selectNames[index])
+                        Image(systemName: selectedIndex == index ? "checkmark.circle.fill" : "circle")
+                            .foregroundColor(.blue)
+                    }
+                    .frame(height: 40)
+                    .onTapGesture {
+                        selectedIndex = index
+                    }
+                })
+            Spacer()
+                    
         }
         .padding()
+    }
+}
+
+struct CheckBoxToggleStyle:ToggleStyle{
+    func makeBody(configuration: Configuration) -> some View {
+        Button{
+            configuration.isOn.toggle()
+        } label: {
+            HStack{
+                configuration.label
+                Spacer()
+                Image(systemName: configuration.isOn
+                      ? "checkmark.square.fill"
+                      : "square")
+            }
+        }
     }
 }
 
