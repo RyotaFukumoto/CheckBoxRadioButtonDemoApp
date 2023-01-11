34b2ba55b43732818686a12e8f6fdeaf495e3684
ryota <k014c1106@it-neec.jp>
2023-01-11 20:41:09 +0900

    Initial Commit

---
+++ CheckBoxRadioButtonDemoApp/CheckBoxRadioButtonDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckBoxRadioButtonDemoAppApp.swift
+//  CheckBoxRadioButtonDemoApp
+//
+//  Created by ryota on 2023/01/11.
+//
+
+import SwiftUI
+
+@main
+struct CheckBoxRadioButtonDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
